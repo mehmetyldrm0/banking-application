940dbd571cf2ca14435e6bf9fc55b580d076d8cb
MEHMET YILDIRIM <mehmety.java@gmail.com>
2024-04-05 02:34:56 +0300

    rest api
    banking application rest api

---
+++ beststore/src/main/java/com/boostmytool/beststore/models/ProductDto.java
@@ -0,0 +1,30 @@
+package com.boostmytool.beststore.models;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class ProductDto {
+
+	@NotEmpty(message = "The name is required")
+	private String name;
+
+	@NotEmpty(message = "The brand is required")
+	private String brand;
+
+	@NotEmpty(message = "The name is required")
+	private String category;
+
+	@Min(0)
+	private double price;
+
+	@Size(min = 10, message = "The description should be at least 10 characters")
+	@Size(max = 2000, message = "The description cannot exceed least 2000 characters")
+	private String description;
+
+	private MultipartFile imageFile;
+}
